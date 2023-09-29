38e8c5883a996d1296ffc713ca04be1d0b4223a3
Vitaliy Bobrovskiy <cool.rinet@yandex.ru>
2023-09-29 20:55:50 +0700

    set up MainViewModel in MainActivity and added UI logic in MainActivity

--- app/src/main/java/com/coolrinet/lab5variant6/MainActivity.kt
+++ app/src/main/java/com/coolrinet/lab5variant6/MainActivity.kt
@@ -2,14 +2,48 @@ package com.coolrinet.lab5variant6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.core.widget.doOnTextChanged
 import com.coolrinet.lab5variant6.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private val mainViewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.apply {
+            numberOfLitersEditText.doOnTextChanged { text, _, _, _ ->
+                mainViewModel.numberOfLitersStr = text.toString()
+            }
+
+            gasolineBrandRadioGroup.setOnCheckedChangeListener { _, checkedId ->
+                mainViewModel.changeCurrentGasolineBrand(checkedId)
+            }
+
+            calculateCostButton.setOnClickListener {
+                if (mainViewModel.numberOfLitersStr.isEmpty()) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        R.string.numbers_of_liters_not_specified_toast,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+
+                if (gasolineBrandRadioGroup.checkedRadioButtonId == -1) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        R.string.gasoline_brand_not_selected_toast,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+            }
+        }
     }
 }
